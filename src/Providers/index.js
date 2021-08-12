667ca5d8b2d65914325acfab7ed02baec7bbfa6b
KaioIwakiri <kaio_ti@hotmail.com>
2021-08-12 14:11:00 -0300

    feat(login): creating login page, token provider, user provider

--- src/Providers/index.js
+++ src/Providers/index.js
@@ -1,17 +1,18 @@
 import { UserProvider } from "./user";
 import { HabitsProvider } from "./habits";
 import { GroupsProvider } from "./groups";
+import { TokenProvider } from "./token";
 
-const Providers = ({children}) => {
-    return (
-        <UserProvider>
-            <HabitsProvider>
-                <GroupsProvider>
-                    {children}
-                </GroupsProvider>
-            </HabitsProvider>
-        </UserProvider>
-    )
-}
+const Providers = ({ children }) => {
+  return (
+    <TokenProvider>
+      <UserProvider>
+        <HabitsProvider>
+          <GroupsProvider>{children}</GroupsProvider>
+        </HabitsProvider>
+      </UserProvider>
+    </TokenProvider>
+  );
+};
 
 export default Providers;
