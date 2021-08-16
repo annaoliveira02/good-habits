1e53df58c33d8fad51f6eab231669e31b1f5b588
KaioIwakiri <kaio_ti@hotmail.com>
2021-08-16 11:41:26 -0300

    Merge branch feat(src/components) into Develop

--- src/pages/DashboardGroups/index.js
+++ src/pages/DashboardGroups/index.js
@@ -6,29 +6,42 @@ import Footer from "../../components/Footer";
 import { useState } from "react";
 import { Drawer } from "@material-ui/core";
 import DrawerMenu from "../../components/DrawerMenu";
+import GroupCreatorPopup from "../../components/GroupCreator";
 
 const DashboardGroups = () => {
+  const [openModal, setOpenModal] = useState(false);
 
-  const [showDrawer, setShowDrawer] = useState(false)
+  const [showDrawer, setShowDrawer] = useState(false);
 
+  const handleOpen = () => {
+    setOpenModal(true);
+  };
+  const handleClose = () => {
+    setOpenModal(false);
+  };
   return (
     <div>
-      <Drawer anchor="left" open={showDrawer} onClose={() => setShowDrawer(false)}>
-        <DrawerMenu/>
+      <Drawer
+        anchor="left"
+        open={showDrawer}
+        onClose={() => setShowDrawer(false)}
+      >
+        <DrawerMenu />
       </Drawer>
-      <Header setShowDrawer={setShowDrawer}/>
+      <Header setShowDrawer={setShowDrawer} />
       <DashboardContainer>
-      <SideMenu/>
-      <DashboardMainBox>
-        <div className="mainHabits">Meus grupos</div>
-        <div className="mainGroups">
-          Criar grupo
-          <button>Novo grupo</button></div>
-      </DashboardMainBox>      
+        <SideMenu />
+        <DashboardMainBox>
+          <div className="mainHabits">Meus grupos</div>
+          <div className="mainGroups">
+            Criar grupo
+            <button>Novo grupo</button>
+          </div>
+        </DashboardMainBox>
       </DashboardContainer>
-      <Footer/>
-    </div> 
-  )
+      <Footer />
+    </div>
+  );
 };
 
 export default DashboardGroups;
