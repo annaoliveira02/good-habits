a2216b9ad9d1dabdecf60b5a8b417dab92378317
KaioIwakiri <kaio_ti@hotmail.com>
2021-08-18 19:31:27 -0300

    feat(drawer mobile): burguer menu no longer appear on landing page, signup and login

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,34 @@
-import { HeaderMain } from "./style"
+import { HeaderMain } from "./style";
 import { useHistory } from "react-router";
 import { MainButton } from "../../styles/mainContainers";
-import { AiOutlineMenu } from "react-icons/ai"
+import { AiOutlineMenu } from "react-icons/ai";
+import { useAuthentication } from "../../Providers/Authentication";
 
-const Header = ({setShowDrawer}) => {
+const Header = ({ setShowDrawer }) => {
+  const { authenticated } = useAuthentication();
 
-    const history = useHistory();
-    
-    return (
-        <HeaderMain>
-            <div className="MenuButton" onClick={() => setShowDrawer(true)}><AiOutlineMenu/></div>
-            <div onClick={() => history.push("/")} className="HeaderLogo">                
-                <h1 className="Logo1">good</h1><h1 className="Logo2">habits.</h1>
-            </div>
-            <div className="HeaderButtons">
-                <MainButton onClick={() => history.push("/Login")}>Login</MainButton>
-                <MainButton onClick={() => history.push("/SignUp")}>Cadastro</MainButton>
-            </div>
-        </HeaderMain>
-    )
-}
+  const history = useHistory();
+
+  return (
+    <HeaderMain>
+      {authenticated && (
+        <div className="MenuButton" onClick={() => setShowDrawer(true)}>
+          <AiOutlineMenu />
+        </div>
+      )}
+
+      <div onClick={() => history.push("/")} className="HeaderLogo">
+        <h1 className="Logo1">good</h1>
+        <h1 className="Logo2">habits.</h1>
+      </div>
+      <div className="HeaderButtons">
+        <MainButton onClick={() => history.push("/Login")}>Login</MainButton>
+        <MainButton onClick={() => history.push("/SignUp")}>
+          Cadastro
+        </MainButton>
+      </div>
+    </HeaderMain>
+  );
+};
 
 export default Header;
