35281e38250a9df80762b03e0a4a52504e382299
DeniseBelo <denise_belo19@yahoo.com.br>
2021-08-19 19:47:45 -0300

    fix(cleaning code): errasing comments

--- src/components/GoalCard/index.js
+++ src/components/GoalCard/index.js
@@ -2,63 +2,62 @@ import { useState, useEffect, useContext } from "react";
 import { GroupsContext } from "../../Providers/groups";
 import { useToken } from "../../Providers/token";
 import api from "../../services/api";
-import { BsCheck } from "react-icons/bs"
-import { AiOutlineDelete } from "react-icons/ai"
+import { BsCheck } from "react-icons/bs";
+import { AiOutlineDelete } from "react-icons/ai";
 import { GoalBox } from "./style";
 import { toast } from "react-toastify";
 
 const GoalCard = ({ goal, setGoalsList, group, goalsList }) => {
   const [isAchieved, setIsAchieved] = useState(goal.achieved);
-  const [dinamicGoal, setDinamicGoal] = useState(goal.title)
+  const [dinamicGoal, setDinamicGoal] = useState(goal.title);
   const [specificGoal, setSpecificGoal] = useState([]);
   const { token } = useToken();
   const { getGroups } = useContext(GroupsContext);
   const config = { headers: { Authorization: `Bearer ${token}` } };
 
   useEffect(() => {
-    setDinamicGoal(goal.title)
-
-  }, [goalsList])
+    setDinamicGoal(goal.title);
+  }, [goalsList]);
 
   const getOneGoal = () => {
     api
       .get(`/goals/${goal.id}/`)
       .then((response) => {
-        setSpecificGoal(response)
-        console.log(specificGoal)
+        setSpecificGoal(response);
+        console.log(specificGoal);
       })
-      .catch((err) => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   const editGoal = () => {
     getOneGoal();
-    const achievedGoal = { "achieved": true }
+    const achievedGoal = { achieved: true };
     api
       .patch(`/goals/${goal.id}/`, achievedGoal, config)
-      .then(res => {
+      .then((res) => {
         toast.success("Meta concluída! Parabéns!", {
           position: "top-right",
           autoClose: 5000,
           hideProgressBar: false,
           closeOnClick: true,
           pauseOnHover: true,
-        })
+        });
         setIsAchieved(true);
         // getOneGoal()
         getGroups();
       })
-      .catch((err) => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   const deleteGoal = () => {
-    const tk = JSON.parse(localStorage.getItem('@gestaohabitosg5:token'));
+    const tk = JSON.parse(localStorage.getItem("@gestaohabitosg5:token"));
     api
       .delete(`/goals/${goal.id}/`, {
-        headers: { Authorization: `Bearer ${tk}` }
+        headers: { Authorization: `Bearer ${tk}` },
       })
       .then((res) => api.get(`/goals/?group=${group.id}`))
       .then((res) => setGoalsList(res.data.results))
-      .then(res => getGroups())
+      .then((res) => getGroups())
       .catch((err) => console.log(err));
   };
 
@@ -66,16 +65,17 @@ const GoalCard = ({ goal, setGoalsList, group, goalsList }) => {
     <GoalBox>
       <div className="goalTitle">{dinamicGoal}</div>
       <div className="goalButtons">
-        {!isAchieved &&
+        {!isAchieved && (
           <button onClick={editGoal}>
             <BsCheck />
-          </button>}
+          </button>
+        )}
         <button onClick={deleteGoal}>
           <AiOutlineDelete />
         </button>
       </div>
     </GoalBox>
-  )
-}
+  );
+};
 
 export default GoalCard;
